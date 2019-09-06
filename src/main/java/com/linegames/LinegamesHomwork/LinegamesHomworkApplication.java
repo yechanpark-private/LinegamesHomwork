97f3a113337ccb746ff24e1eed933219b8c0ffa1
yechanpark-private <qkrdpcks0419@gmail.com>
2019-09-06 20:54:19 +0900

    Initial Commit

---
+++ src/main/java/com/linegames/LinegamesHomwork/LinegamesHomworkApplication.java
@@ -0,0 +1,13 @@
+package com.linegames.LinegamesHomwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinegamesHomworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinegamesHomworkApplication.class, args);
+	}
+
+}
