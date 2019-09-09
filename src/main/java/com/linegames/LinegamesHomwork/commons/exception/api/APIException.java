c618d2d96f275d2369c8c6e0a4f65d1fb5b5c2dd
yechanpark-private <qkrdpcks0419@gmail.com>
2019-09-09 18:52:57 +0900

    정보 수정 페이지 ajax 작업 추가 / extended Exception Class에 @EqualsAndHashCode 추가

--- src/main/java/com/linegames/LinegamesHomwork/commons/exception/api/APIException.java
+++ src/main/java/com/linegames/LinegamesHomwork/commons/exception/api/APIException.java
@@ -2,6 +2,7 @@ package com.linegames.LinegamesHomwork.commons.exception.api;
 
 import com.linegames.LinegamesHomwork.commons.exception.ErrorCodeEmum;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
@@ -9,6 +10,7 @@ import java.io.Serializable;
  * API Exception 최상위 클래스
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class APIException extends RuntimeException implements Serializable {
     private ErrorCodeEmum errorCodeEmum;
 
--- src/main/java/com/linegames/LinegamesHomwork/commons/exception/web/WebException.java
+++ src/main/java/com/linegames/LinegamesHomwork/commons/exception/web/WebException.java
@@ -1,10 +1,12 @@
 package com.linegames.LinegamesHomwork.commons.exception.web;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class WebException extends Exception implements Serializable {
     private String errorMessage;
     private String redirectURL;
