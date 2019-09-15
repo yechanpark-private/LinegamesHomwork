0370fa06e0398836f65d743e4b07a8b5904d6c35
yechanpark-private <qkrdpcks0419@gmail.com>
2019-09-15 13:51:42 +0900

    UserRepository @Transactional 추가

--- src/main/java/com/linegames/LinegamesHomwork/auth/repository/UserRepository.java
+++ src/main/java/com/linegames/LinegamesHomwork/auth/repository/UserRepository.java
@@ -4,10 +4,13 @@ import com.linegames.LinegamesHomwork.auth.model.CustomUserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 /**
  * {@link CustomUserDetails} 클래스에 대한 Repository
  */
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<CustomUserDetails, Long> {
     CustomUserDetails findByUsername(String username);
 }
